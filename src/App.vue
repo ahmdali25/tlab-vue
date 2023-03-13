0258c42921de2a60bee1594c88ecd1a7ae4a7de4
ahmdali25 <ahmadaliazhary@gmail.com>
2023-03-13 18:22:42 +0700

    ADD css vars and component base header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BaseHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import BaseHeader from "./components/BaseHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    BaseHeader,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-left: 100px;
+  padding-right: 100px;
 }
 </style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav>
+    <div class="nav">
+      <div>
+        <img src="../assets/images/brand_logo.svg" alt="TLab Logo" />
+      </div>
+      <div>
+        <ul class="nav-list">
+          <li>Company</li>
+          <li>Layanan</li>
+          <li>Studi Kasus</li>
+          <li>Data Center</li>
+        </ul>
+      </div>
+      <div>
+        <button class="btn-nav">Hubungi Kami</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "BaseHeader",
+};
+</script>
+
+<style scoped>
+.nav {
+  display: grid;
+  grid-template-columns: 200px 800px 200px;
+  padding: 10px;
+  align-items: center;
+}
+
+.nav-list {
+  list-style: none;
+  font-size: 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+}
+
+.nav-list li {
+  cursor: pointer;
+}
+
+.btn-nav {
+  font-size: 16px;
+  padding: 12px 24px;
+  height: 48px;
+  background: var(--color-primary);
+  color: #ffffff;
+  box-shadow: 0px 2px 8px rgba(25, 137, 104, 0.19);
+  border-radius: 100px;
+  border: none;
+  cursor: pointer;
+}
+</style>
